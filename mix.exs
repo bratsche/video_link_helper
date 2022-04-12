5f0a8e6f90252efa0a48edf44e9fe3d124673663
Cody Russell <cody@jhu.edu>
2022-04-11 23:36:04 -0500

    Package info.

--- mix.exs
+++ mix.exs
@@ -1,20 +1,25 @@
 defmodule VideoLinkHelper.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Elixir utility library for dealing with embedded Youtube and Vimeo videos"
+
   def project do
     [
       app: :video_link_helper,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/bratsche/video_link_helper"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
     ]
   end
 
@@ -23,6 +28,15 @@ defmodule VideoLinkHelper.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/bratsche/video_link_helper"
+      }
     ]
   end
 end
