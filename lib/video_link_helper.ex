c1a6f7a41121ddbd66afe7b4332eca9b6deb171f
Cody Russell <cody@jhu.edu>
2022-10-02 00:37:31 -0400

    New VideoLinkHelper.render_embed/2 function.

--- lib/video_link_helper.ex
+++ lib/video_link_helper.ex
@@ -27,15 +27,54 @@ defmodule VideoLinkHelper do
     end
   end
 
-  def create_youtube_embed(id, height, width) do
+  @default_width 560
+  @default_height 315
+  @default_type :youtube
+
+  @doc """
+  Renders HTML for a video embed.
+
+  ## Options
+
+  * `:width` - Width of the video embed. Default is 560.
+  * `:height` - Height of the video embed. Default is 315.
+  * `:video_type` - The type of video embed to render. Must be
+  either `:youtube` or `:vimeo`. If this option is not provided,
+  the default is `:youtube`.
+
+  Returns a string.
+
+  """
+  @doc since: "0.2.0"
+  def render_embed(id, options \\ []) do
+    width = Keyword.get(options, :width, @default_width)
+    height = Keyword.get(options, :height, @default_height)
+    video_type = Keyword.get(options, :video_type, @default_type)
+
+    create_embed(id, height, width, to_string(video_type))
+  end
+
+  @doc deprecated: "Use VideoLinkHelper.render_embed/2 instead."
+  def create_youtube_embed(id, height, width), do: create_embed(id, height, width, "youtube")
+
+  @doc deprecated: "Use VideoLinkHelper.render_embed/2 instead."
+  def create_vimeo_embed(id, height, width), do: create_embed(id, height, width, "vimeo")
+
+  # -- private functions
+
+  defp create_embed(id, height, width, "youtube") do
     """
-    <iframe width="#{width}" height="#{height}" src="//www.youtube.com/embed/#{id}" style="border: none;" allowfullscreen></iframe>
+    <iframe width="#{width}" height="#{height}" src="https://www.youtube.com/embed/#{id}" style="border: none;" allowfullscreen></iframe>
     """
   end
 
-  def create_vimeo_embed(id, height, width) do
+  defp create_embed(id, height, width, "vimeo") do
     """
-    <iframe height="#{height}" width="#{width}" src="//player.vimeo.com/video/#{id}" style="border: none;" allowfullscreen></iframe>
+    <iframe width="#{width}" height="#{height}" src="https://player.vimeo.com/video/#{id}" style="border: none;" allowfullscreen></iframe>
     """
+  end
+
+  defp create_embed(_id, _height, _width, video_type) do
+    raise "Invalid video type: #{inspect(video_type)}"
   end
 end
